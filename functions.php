27a5ac66e5d6b92ae48d107e6c3ad7ce321e5436
Raffol <eremenko97@bk.ru>
2025-11-27 22:39:59 +0800

    feat: wp event in head

--- functions.php
+++ functions.php
@@ -1,1 +1,69 @@
 <?php
+//Подключение стилей
+
+// Подключение стилей темы
+add_action('wp_enqueue_scripts', 'testprojectwp_script');
+function testprojectwp_script()
+{
+    // Подключаем Bootstrap
+    wp_enqueue_style(
+        'bootstrap',
+        get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.css',
+        [],
+        null
+    );
+
+    // Flaticon
+    wp_enqueue_style(
+        'flaticon',
+        get_template_directory_uri() . '/assets/fonts/flaticon/flaticon.css',
+        [],
+        null
+    );
+
+    // Font Awesome (all.css)
+    wp_enqueue_style(
+        'fontawesome-all',
+        get_template_directory_uri() . '/assets/css/all.css',
+        [],
+        null
+    );
+
+    // Icofont
+    wp_enqueue_style(
+        'icofont',
+        get_template_directory_uri() . '/assets/css/icofont.css',
+        [],
+        null
+    );
+
+    // Animate.css
+    wp_enqueue_style(
+        'animate',
+        get_template_directory_uri() . '/assets/css/animate.min.css',
+        [],
+        null
+    );
+
+    // Основной style.css шаблона
+    wp_enqueue_style(
+        'theme-style',
+        get_template_directory_uri() . '/assets/css/style.css',
+        [],
+        filemtime(get_template_directory() . '/assets/css/style.css') // чтобы обновлялось без кеша
+    );
+
+    // Responsive
+    wp_enqueue_style(
+        'responsive',
+        get_template_directory_uri() . '/assets/css/responsive.css',
+        [], // зависит от основного
+        null
+    );
+
+    // style.css из корня темы – WP стандарт
+    wp_enqueue_style(
+        'main-style',
+        get_stylesheet_uri()
+    );
+}
