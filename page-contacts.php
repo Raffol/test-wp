88343126e3bd5a079a19be689670692d0ede7607
Raffol <eremenko97@bk.ru>
2025-12-05 10:07:40 +0800

    feat: content

--- page-contacts.php
+++ page-contacts.php
@@ -27,4 +27,32 @@ Template Post Type: post, page, product, contacts
     </div>
 </div>
 <!--MAIN HEADER AREA END -->
+<?php the_content();?>
+<!-- HTML для вставки на страницу -->
+<div id="map-yandex" style="width: 100%; height: 400px;"></div>
+
+<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU&apikey=ваш_api_ключ"></script>
+<script>
+    ymaps.ready(function() {
+        var map = new ymaps.Map("map-yandex", {
+            center: [52.275140, 104.281137], // Координаты ИРНИТУ
+            zoom: 16
+        });
+
+        var marker = new ymaps.Placemark([52.275140, 104.281137], {
+            balloonContent: 'Иркутский национальный исследовательский технический университет (ИРНИТУ)'
+        }, {
+            preset: 'islands#blueEducationIcon'
+        });
+
+        map.geoObjects.add(marker);
+        map.controls.remove('geolocationControl');
+        map.controls.remove('searchControl');
+        map.controls.remove('trafficControl');
+        map.controls.remove('typeSelector');
+        map.controls.remove('fullscreenControl');
+        map.controls.remove('rulerControl');
+    });
+</script>
+
 <?php get_footer();?>
